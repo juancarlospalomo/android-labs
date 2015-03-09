076a128d4c7e13ab646b6e3a218c08e447d5ade2
Juan Carlos Palomo <juancarlos.palomo@gmail.com>
2015-03-09 22:39:43 +0100

    Toolbar Style

---
+++ Toolbar-Styled/app/src/main/java/com/applilandia/toolbarstyled/MainActivity.java
@@ -0,0 +1,52 @@
+package com.applilandia.toolbarstyled;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private Toolbar mToolBar;
+    private boolean mShowMenu = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mToolBar = (Toolbar) findViewById(R.id.toolBarMain);
+        setSupportActionBar(mToolBar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        if (mShowMenu) getMenuInflater().inflate(R.menu.menu_main, menu);
+        return mShowMenu;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            mShowMenu = !mShowMenu;
+            invalidateOptionsMenu();
+        }
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
